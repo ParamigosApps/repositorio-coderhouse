d4ca869c28c8b16b31acf45ce6c63d691c037b45
Ivan <ivangabrielruiz1@gmail.com>
2025-10-01 23:47:28 -0300

    se añade buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,2 +1,46 @@
-const buscador = document.getElementById("buscador");
-const productosContainer = document.getElementById("productos-container");
+const inputBuscador = document.getElementById("buscador");
+const contenedorProductos = document.getElementById("productos-container");
+const avisoBuscador = document.getElementById("aviso-buscador");
+
+inputBuscador.addEventListener("keyup", () => {
+  const valorBuscado = inputBuscador.value.toLowerCase();
+
+  const productos = contenedorProductos.getElementsByClassName(
+    "product-description-title"
+  );
+
+  let sinResultados = true;
+  Array.from(productos).forEach((producto) => {
+    const card = producto.parentElement;
+    const textoProducto = producto.textContent.toLowerCase();
+
+    if (textoProducto.includes(valorBuscado)) {
+      card.style.display = "block";
+      sinResultados = false;
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  if (sinResultados) {
+    avisoBuscador.textContent = `Sin coincidencias para "${valorBuscado}"`;
+    avisoBuscador.style.display = "block";
+  } else {
+    avisoBuscador.style.display = "none";
+  }
+});
+
+inputBuscador.addEventListener("input", () => {
+  if (inputBuscador.value === "") {
+    avisoBuscador.style.display = "none";
+
+    const productos = contenedorProductos.getElementsByClassName(
+      "product-description-title"
+    );
+
+    Array.from(productos).forEach((producto) => {
+      const card = producto.parentElement;
+      card.style.display = "block";
+    });
+  }
+});
